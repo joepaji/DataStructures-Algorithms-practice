4485417696ea02693265efc84b2702b2b04fbc11
Joheb R <rjoheb@hotmail.com>
2020-08-29 14:50:54 -0700

    reverse string using reduce function

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,23 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+//********Solution 3 using reduce function - Advanced********//
+
 function reverse(str){
+    return str.split('').reduce((rev, char)=>
+    char + rev, '');    
+}
+
+module.exports = reverse;
+
+//******Solution 1 using reverse function - easy********//
+
+/*function reverse(str) {
+    let reverse;
+    reverse = str.split('').reverse().join('');
+    
+    return reverse;
+}*/
+
+//********Solution 2 using new for loop syntax - moderate********//
+
+/*function reverse(str){
     let reverse = '';
 
     for (let character of str){
         reverse = character + reverse;
     }
 
-    return reverse;
-}
-
-module.exports = reverse;
-
-//Solution 1 using reverse function
-
-/*function reverse(str) {
-    let reverse;
-    reverse = str.split('').reverse().join('');
-    
     return reverse;
 }*/
